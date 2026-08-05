b64427fd745001c90ac4452888844c8d817146b7
David Tarek <davidelmohalek@gmail.com>
2026-08-05 19:21:32 +0300

    Complete variables exercises

---
+++ 01-fundamentals/02-variables-data-types/Basic Data Types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int integerVar;
+  long long longVar;
+  char charVar;
+  float floatVar;
+  double doubleVar;
+
+  cin >> integerVar;
+  cin >> longVar;
+  cin >> charVar;
+  cin >> floatVar;
+  cin >> doubleVar;
+
+  cout << integerVar << endl;
+  cout << longVar << endl;
+  cout << charVar << endl;
+  cout << floatVar << endl;
+  cout << doubleVar << endl;
+
+  return 0;
+}
